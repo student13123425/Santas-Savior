b56fa00d80b1a49922c1609cd1dcb9235b9e8306
NicolaeMihaiAlexandru <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-12-21 12:26:11 +0200

    Updated Texture Object to apply offsets properly

--- ConsoleApp1/TextureObject.cs
+++ ConsoleApp1/TextureObject.cs
@@ -18,40 +18,45 @@ namespace ConsoleApp1
             Offset = offset ?? new Vec2D(0, 0);
         }
 
+        private Vec2D ApplyOffset(Vec2D position, bool flip)
+        {
+            return flip ? position - Offset : position + Offset;
+        }
+
         public float DrawCenter(float pixelSize, bool isWidth, Vec2D position, bool flip = false, float rotation = 0f)
         {
-            return _renderer.DrawTextureCenter(Texture, pixelSize, isWidth, position + Offset, flip, rotation);
+            return _renderer.DrawTextureCenter(Texture, pixelSize, isWidth, ApplyOffset(position, flip), flip, rotation);
         }
 
         public float DrawBottomCenter(float pixelSize, bool isWidth, Vec2D position, bool flip = false)
         {
-            return _renderer.DrawTextureBottomCenter(Texture, pixelSize, isWidth, position + Offset, flip);
+            return _renderer.DrawTextureBottomCenter(Texture, pixelSize, isWidth, ApplyOffset(position, flip), flip);
         }
 
         public float DrawTopCenter(float pixelSize, bool isWidth, Vec2D position, bool flip = false)
         {
-            return _renderer.DrawTextureTopCenter(Texture, pixelSize, isWidth, position + Offset, flip);
+            return _renderer.DrawTextureTopCenter(Texture, pixelSize, isWidth, ApplyOffset(position, flip), flip);
         }
 
         public float DrawLeftCenter(float pixelSize, bool isWidth, Vec2D position, bool flip = false)
         {
-            return _renderer.DrawTextureLeftCenter(Texture, pixelSize, isWidth, position + Offset, flip);
+            return _renderer.DrawTextureLeftCenter(Texture, pixelSize, isWidth, ApplyOffset(position, flip), flip);
         }
 
         public float DrawRightCenter(float pixelSize, bool isWidth, Vec2D position, bool flip = false)
         {
-            return _renderer.DrawTextureRightCenter(Texture, pixelSize, isWidth, position + Offset, flip);
+            return _renderer.DrawTextureRightCenter(Texture, pixelSize, isWidth, ApplyOffset(position, flip), flip);
         }
 
         public void DrawRect(Rect2D rect, bool flip = false, float rotation = 0f)
         {
-            Rect2D offsetRect = new Rect2D(rect.Pos + Offset, rect.Size);
+            Rect2D offsetRect = new Rect2D(ApplyOffset(rect.Pos, flip), rect.Size);
             _renderer.DrawTextureRect(Texture, offsetRect, flip, rotation);
         }
 
         public void DrawRectCentered(Rect2D rect, bool flip = false, float rotation = 0f)
         {
-            Rect2D offsetRect = new Rect2D(rect.Pos + Offset, rect.Size);
+            Rect2D offsetRect = new Rect2D(ApplyOffset(rect.Pos, flip), rect.Size);
             _renderer.DrawTextureRectCentered(Texture, offsetRect, flip, rotation);
         }
     }
